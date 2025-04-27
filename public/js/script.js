36197d37d0a71e3d6143bc9b692e2ff3e25dbf60
MancillaO <edgarmncllav04@gmail.com>
2025-04-26 23:44:24 -0600

    add config file and update API import path in script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,4 @@
-import { API_BASE_URL } from '../../config.js'
+import { API_BASE_URL } from '../js/config.js'
 
 document.addEventListener('DOMContentLoaded', function () {
   // Función para construir URLs específicas de la API
